e0a51c59925ed31b7ece9d1f79130a798c3fe580
unknown <razzytirta@gmail.com>
2023-07-17 09:58:39 +0800

    Belajar Dart Packages Library 2023

---
+++ example/hello.dart
@@ -0,0 +1,9 @@
+import 'package:belajar_dart_packages_library_2023/hello.dart';
+
+void main() {
+  print(sayHello('Fachrurazzi'));
+
+  var customer = Customer();
+  var category = Category();
+  var product = Product();
+}
